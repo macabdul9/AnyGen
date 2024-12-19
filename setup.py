edc6c2f6a4eb4f1a10a65216008bf3d36ebc47d5
Abdul Waheed <abdulwaheed1513@gmail.com>
2024-12-19 18:05:41 -0500

    github actions and new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="anygen",
-    version="1.0.0",
+    version="1.0.1",
     author="Abdul Waheed",
     author_email="abdulwaheed1513@gmail.com",
     description="A unified interface for text generation using Hugging Face, OpenAI, and Gemini models.",
